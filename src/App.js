ce924497965b3e5266101278f2d502555a6a6c77
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-01 17:46:58 -0400

    Trying again aaaaaaaaaa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routes from './routes';
+import Pokemons from './pages/Pokemons';
 
 
 function App() {
   return (
-    <Routes />    
+    // <Routes />    
+    <Pokemons />
   );
 }
 
