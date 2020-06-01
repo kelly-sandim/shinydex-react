313f55aa9afccc0986e89ac867915b455e8ea25c
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-01 19:09:46 -0400

    aaaaaaaaaaa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Routes from './routes';
-import Pokemons from './pages/Pokemons';
 
 
 function App() {
   return (
-    // <Routes />    
-    <Pokemons />
+    <Routes />    
   );
 }
 
